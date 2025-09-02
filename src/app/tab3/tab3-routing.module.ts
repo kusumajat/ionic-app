40b188107c3abe7bc1c4aa5b5496f41b55dd0a65
Kusumajati <rkusumajati14@gmail.com>
2025-09-02 18:43:28 +0700

    refactor(tab3-routing): update routing to use loadComponent for Tab3Page

--- src/app/tab3/tab3-routing.module.ts
+++ src/app/tab3/tab3-routing.module.ts
@@ -1,5 +1,3 @@
-// File: src/app/tab3/tab3-routing.module.ts
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab3Page } from './tab3.page';
@@ -7,13 +5,6 @@ import { Tab3Page } from './tab3.page';
 const routes: Routes = [
   {
     path: '',
-    // --- UBAH BARIS DI BAWAH INI ---
-    // Awal (Sebelum diubah):
-    // component: Tab3Page,
-    // atau
-    // loadChildren: () => import('./tab3.module').then(m => m.Tab3PageModule)
-
-    // Menjadi (Setelah diubah):
     loadComponent: () => import('./tab3.page').then((m) => m.Tab3Page),
   },
 ];
