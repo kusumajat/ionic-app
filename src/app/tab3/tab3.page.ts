3d3be53cd34c066c9511ded647fb0805cee0d84e
Kusumajati <rkusumajati14@gmail.com>
2025-09-02 23:26:30 +0700

    refactor(tab3): restructure Tab3 module and update routing to use loadChildren

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,24 +1,7 @@
 // File: src/app/tab3/tab3.page.ts
 
 import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonButton,
-  IonActionSheet,
-  IonList,
-  IonItem,
-  IonBadge,
-  IonLabel,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
-  IonDatetime,
-  IonInput
-} from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -26,25 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [
-    CommonModule,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButton,
-    IonActionSheet,
-    IonList,
-    IonItem,
-    IonBadge,
-    IonLabel,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    IonDatetime,
-    IonInput
-  ],
+  imports: [IonicModule, CommonModule],
 })
 export class Tab3Page {
   public actionSheetButtons = [
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
       },
       {
         path: 'tab3',
-        loadComponent: () => import('../tab3/tab3.page').then(m => m.Tab3Page)
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
         path: '',
