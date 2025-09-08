90a6d02349b0fafe5717536852a67b5e1e657bd7
Kusumajati <rkusumajati14@gmail.com>
2025-09-08 19:47:38 +0700

    feat(login): implement login page with routing and validation feat(splashscreen): create splashscreen page with auto-navigation refactor(tabs-routing): update routing paths for main navigation

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+
+  email: string = '';
+  password: string = '';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    if (this.email === 'admin' && this.password === 'admin') {
+      window.location.href = '';
+    } else {
+      alert('Email atau password salah. Silakan coba lagi.');
+    }
+  }
+
+}
