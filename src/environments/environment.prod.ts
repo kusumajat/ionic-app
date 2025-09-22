1b2706b1448d06388e1a7263743ba1731a8068e3
Kusumajati <rkusumajati14@gmail.com>
2025-09-22 11:15:02 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
 
   // Your web app's Firebase configuration
   firebaseConfig: {
-    apiKey: 'AIzaSyDUSYwFHB3VcXNNn7yuheokBkkU4GOBal0',
-    authDomain: 'pgpbl-ion.firebaseapp.com',
-    projectId: 'pgpbl-ion',
-    storageBucket: 'pgpbl-ion.firebasestorage.app',
-    messagingSenderId: '519248307536',
-    appId: '1:519248307536:web:9733e23ddc5b9a8e793855',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 };
